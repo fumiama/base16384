ea8740ed4ca46dcdb3a41d61166fa859a46cab56
源文雨 <41315874+fumiama@users.noreply.github.com>
2024-04-06 15:04:44 +0900

    chore: optimize documents

--- base16384.c
+++ base16384.c
@@ -52,11 +52,11 @@ static base16384_err_t print_usage() {
 			BASE16384_VERSION_DATE
 		"). Usage:\n", stderr
 	);
-	fputs("base16384 [-edtn] [inputfile] [outputfile]\n", stderr);
+	fputs("base16384 -[ed][t][n][cC] [inputfile] [outputfile]\n", stderr);
 	fputs("  -e\t\tencode (default)\n", stderr);
 	fputs("  -d\t\tdecode\n", stderr);
 	fputs("  -t\t\tshow spend time\n", stderr);
-	fputs("  -n\t\tdon't write utf16be file header (0xFEFF)\n", stderr);
+	fputs("  -n\t\tdonot write utf16be file header (0xFEFF)\n", stderr);
 	fputs("  -c\t\tembed or validate checksum in remainder\n", stderr);
 	fputs("  -C\t\tdo -c forcely\n", stderr);
 	fputs("  inputfile\tpass - to read from stdin\n", stderr);
